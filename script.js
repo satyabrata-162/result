04c7e4c0c329d434fe654c28c2a45d6bbc8b1771
satyabrata-162 <samantaakash984@gmail.com>
2026-04-01 13:15:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showResult() {
+    document.getElementById("loginBox").style.display = "none";
+    document.getElementById("resultBox").style.display = "block";
+}
+
+function revealPrank() {
+    alert("😂 APRIL FOOL ❤️");
+}
